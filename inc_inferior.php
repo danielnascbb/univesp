71b64318b25c71bbc7de222ade354032b0d459c8
danielnascbb <danielnascbb@gmail.com>
2022-05-01 17:58:04 -0300

    Correção de número de grupo

--- inc_inferior.php
+++ inc_inferior.php
@@ -4,6 +4,6 @@ include 'inc_javas.php';
 ?>
 <div class="col">
     
-    <p class="h6"><br><br>Projeto Integrador em Computação I-PJI110-0033 - Alunos da <a href="http://www.univesp.br" target="_blank">Univesp.br</a></p><br>
+    <p class="h6"><br><br>Projeto Integrador em Computação II- PJI240 / Sala 7 / Grupo 111 - Alunos da <a href="http://www.univesp.br" target="_blank">Univesp.br</a></p><br>
 
 </div>
